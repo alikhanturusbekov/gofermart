e495b35392cf920a1e32774efb429fc2429c4106
Alikhan Turusbekov <turusbekov@kolesa.kz>
2026-02-03 21:53:18 +0500

    Заменил запуск миграции через golang-migrate/migrate вместо ручного создания схемы миграции

--- internal/setup/database.go
+++ internal/setup/database.go
@@ -2,10 +2,10 @@ package setup
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
-	"path/filepath"
-
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -29,47 +29,24 @@ func PrepareDatabase(config *Config) (*sql.DB, error) {
 	return database, nil
 }
 
-// applyMigrations reads the migrations file and applies changes to database
+// applyMigrations runs migrations on the database
 func applyMigrations(db *sql.DB, dir string) error {
-	_, err := db.Exec(`
-        CREATE TABLE IF NOT EXISTS schema_migrations (
-            version VARCHAR(255) PRIMARY KEY
-        );
-    `)
-	if err != nil {
-		return fmt.Errorf("failed to create schema_migrations table: %w", err)
-	}
-
-	files, err := filepath.Glob(filepath.Join(dir, "*.up.sql"))
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
-		version := filepath.Base(file)
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+dir,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
 
-		var exists string
-		err := db.QueryRow("SELECT version FROM schema_migrations WHERE version=$1", version).Scan(&exists)
-		if err == nil {
-			continue
-		} else if err != sql.ErrNoRows {
-			return fmt.Errorf("failed to check migration %s: %w", version, err)
-		}
-
-		sqlBytes, err := os.ReadFile(file)
-		if err != nil {
-			return fmt.Errorf("failed to read migration file %s: %w", file, err)
-		}
-
-		_, err = db.Exec(string(sqlBytes))
-		if err != nil {
-			return fmt.Errorf("failed to apply migration %s: %w", file, err)
-		}
-
-		_, err = db.Exec("INSERT INTO schema_migrations(version) VALUES ($1)", version)
-		if err != nil {
-			return fmt.Errorf("failed to record applied migration %s: %w", version, err)
-		}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
 	}
 
 	return nil
